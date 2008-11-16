6c3ee936a0aa92c7e25b746e70d5f1211d999747
ange.albertini <ange.albertini@2aaee85a-a46a-11dd-b47b-0378ca0a891c>
2008-11-16 22:08:26 +0000

    lzw cleanup

--- trunk/lzw.py
+++ trunk/lzw.py
@@ -13,22 +13,21 @@ def compress(data_to_compress):
     offset = 0
     compressed_data = []
     roots = list(set(data_to_compress))
-    dictionary = roots[:]
+    dictionary = list(roots)   # at the start, the dictionary contains all possible symbols
 
-    length = len(data_to_compress)
     word = ''
-    while offset < length:
-
-        char = data_to_compress[offset]
-
-        if (word + char) in dictionary:
-            word = word + char
+    for char in data_to_compress:
+        if (word + char) in dictionary: # expand word with the next char
+            word = word + char          # as long as it's present in the dictionary
         else:
+            # write down the last corresponding entry in the dictionary
             compressed_data.append(dictionary.index(word))
+            #create new entry with extra char
             dictionary.append(word + char)
+            #resume loop with that char
             word = char
         offset += 1
-    compressed_data.append(dictionary.index(word))
+    compressed_data.append(dictionary.index(word)) # don't forget the remaining word
 
     return roots, compressed_data
 
@@ -37,29 +36,38 @@ assert compress("abracadabra") == (['a', 'r', 'b', 'c', 'd'],
 
 
 def decompress(roots, compressed_data):
-    decompressed_data = ""
-    dictionary = roots
+    dictionary = list(roots)
 
     #the first index has to be one of the root. decompressed_data is started.
-    current_index = compressed_data[0]
-    decompressed_data += dictionary[current_index]
+    first_index = compressed_data[0]
+    first_char = dictionary[first_index]
+    decompressed_data = first_char
+    
+    current_index = first_index
 
     #now we'll read all the remaining indexes, and remember the previous one
     for index_ in compressed_data[1:]:
         previous_index = current_index
         current_index = index_
-        word = dictionary[previous_index]
 
-        if current_index < len(dictionary): # if the index is within the dictionary size,
-            char = dictionary[current_index][0]
+        previous_word = dictionary[previous_index]
+        previous_initial = previous_word[0]
 
-            decompressed_data += dictionary[current_index]
+        if current_index < len(dictionary):                # existing entry ?
+
+            current_word = dictionary[current_index]
+            current_initial = dictionary[current_index][0]
+
+            new_word = previous_word + current_initial
+            decompressed_data += current_word
+            
+            dictionary.append(new_word)
         else:
-            char = dictionary[previous_index][0]
+            # current_index == len(dictionary) except for last iteration
+            new_word = previous_word + previous_initial
+            decompressed_data += new_word
 
-            decompressed_data += word + char
-
-        dictionary.append(word + char)
+            dictionary.append(new_word)
 
     return decompressed_data
 
