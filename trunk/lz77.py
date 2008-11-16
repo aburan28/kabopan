a2ced222011577d8868869a80be5c3d2e6d41a79
ange.albertini <ange.albertini@2aaee85a-a46a-11dd-b47b-0378ca0a891c>
2008-11-16 18:29:32 +0000

    lz77, lz78

---
+++ trunk/lz77.py
@@ -0,0 +1,48 @@
+#Lempel Ziv 77 -  LZ77 - LZ1
+#lossless dictionary (sliding window) coder (compression algorithm)
+#A Universal Algorithm for Sequential Data Compression
+#Jacob Ziv and Abraham Lempel, 1977
+#
+#Kabopan (http://kabopan.corkami.com) public domain, readable, working pseudocode-style python
+#Ange Albertini
+
+import _lz77
+
+def compress(data_to_compress):
+    offset = 0
+    compressed_data = []
+
+    length = len(data_to_compress)
+
+    while offset < length:
+        #find the longest match for the next word to encode
+        match_offset, match_length = _lz77.find_longest_match(data_to_compress[:offset],
+                         data_to_compress[offset:])
+        
+        #get the next symbol, if there is something else to compress
+        symbol = data_to_compress[offset + match_length] if offset + match_length < length else ''
+
+        compressed_data.append({"offset": match_offset, "length":match_length, "symbol":symbol})
+        offset += match_length + 1 # +1 since the next char is included in symbol
+
+    return compressed_data
+
+assert compress("abracadabra") == [{'length': 0, 'offset': 0, 'symbol': 'a'},
+                                    {'length': 0, 'offset': 0, 'symbol': 'b'},
+                                    {'length': 0, 'offset': 0, 'symbol': 'r'},
+                                    {'length': 1, 'offset': 3, 'symbol': 'c'},
+                                    {'length': 1, 'offset': 2, 'symbol': 'd'},
+                                    {'length': 4, 'offset': 7, 'symbol': ''}] 
+
+def decompress(compressed_data):
+    decompressed_string = ""
+
+    for d in compressed_data:
+        if d["length"] != 0:
+            for i in xrange(d["length"]):
+                decompressed_string += decompressed_string[-d["offset"]]
+        decompressed_string += d["symbol"]
+
+    return decompressed_string
+
+assert decompress(compress("abracadabra")) == "abracadabra"
