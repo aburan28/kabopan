2455b6dc42f06aa1a5d6b160c02ab2d1fe9d40de
ange.albertini <ange.albertini@2aaee85a-a46a-11dd-b47b-0378ca0a891c>
2008-11-16 22:52:17 +0000

    added shannon fano and entropy encoder functions

---
+++ trunk/_encoding.py
@@ -0,0 +1,30 @@
+#Useful Functions for entropy encoding algorithms, such as Shannon Fano or Huffman
+#
+#Kabopan (http://kabopan.corkami.com) public domain, readable, working pseudocode-style python
+#Ange Albertini
+
+def generate_codes(node, current_code=""):
+    """walk the encoding tree and generates code for symbol (in leaves)"""
+    codes = []
+    if "symbol" in node:
+        return node["symbol"], current_code
+    else:
+        codes.extend(generate_codes(node["left0"], current_code + "0"))
+        codes.extend(generate_codes(node["right1"], current_code + "1"))
+    return codes
+
+assert generate_codes({'left0': {'symbol': 'a', 'weight': 1},
+                     'right1': {'symbol': 'b', 'weight': 1},
+                     'weight': 2}) ==["a", "0", "b", "1"]
+
+def get_weights_and_symbols(data):
+    stats = [{"symbol": chr(i), "weight": 0} for i in range(256)]
+    for char in data:
+        stats[ord(char)]["weight"] += 1
+    stats = [i for i in stats if i["weight"] > 0]
+    return stats
+
+assert get_weights_and_symbols("a") == [{"symbol":"a","weight":1}]
+assert get_weights_and_symbols("abababbc") == [{"symbol":"a","weight":3},
+                                               {"symbol":"b","weight":4},
+                                               {"symbol":"c","weight":1},]
---
+++ trunk/sfc.py
@@ -0,0 +1,77 @@
+#Shannon-Fano coding
+#Entropy encoding
+#A Mathematical Theory of Communication
+#Claude E. Shannon, 1948
+#
+#Kabopan (http://kabopan.corkami.com) public domain, readable, working pseudocode-style python
+#Ange Albertini
+
+import _encoding
+
+def split(list, index):
+    """split the list in 2 halves AFTER the index parameter"""
+    return list[:index + 1], list[index + 1:]
+
+assert split([0,1,2,3],1) == ([0,1], [2,3])
+assert split([0,1,2,3],0) == ([0], [1,2,3])
+
+def split_weights(weights):
+    """returns the index of a list of weight, after which the list is split in 'almost equal' halves
+    ie when the difference of cumulated weights of both parts is minimal"""
+    length = len(weights)
+
+    increasing_weights = [sum(weights[:i + 1]) for i in range(length)]
+    decreasing_weights = [sum(weights[i:]) for i in range(length)]
+    differences = [abs(increasing_weights[i] - decreasing_weights[i + 1])
+                   for i in range(length - 1)]
+
+    half_weight = min(differences)
+    half_index = differences.index(min(differences))
+    return half_index
+
+assert split_weights([1,1]) == 0
+assert split_weights([1,2,3]) == 1
+assert split_weights([8,7,1]) == 0
+assert split_weights([1,1,8]) == 1
+
+
+def generate_sfc_tree(elements):
+    """generate a shannon fano tree out of a list of dictionaries of symbols and their weights"""
+    if len(elements) == 1:
+        node = {"symbol":elements[0]["symbol"]} # nothing to split - it's a leaf
+    else:
+        weights = [e["weight"] for e in elements]
+        half_weight_index = split_weights(weights)
+
+        left_half, right_half = split(elements, half_weight_index)
+
+        left_node = generate_sfc_tree(left_half)
+        right_node = generate_sfc_tree(right_half)
+
+        node = {"left0": left_node, "right1": right_node}
+
+    return node
+
+
+assert generate_sfc_tree([{"symbol":"a", "weight":35}]) == {"symbol":"a"}
+assert generate_sfc_tree([{"symbol":"a", "weight":2},
+                          {"symbol":"b", "weight":2}]) == {'left0': {'symbol': 'a'},
+                                                           'right1': {'symbol': 'b'}}
+
+def encode(data_to_compress):
+    stats = _encoding.get_weights_and_symbols(data_to_compress)    
+
+    stats = sorted(stats, key = lambda x:x["weight"], reverse = True)
+
+    tree = generate_sfc_tree(stats)
+
+    return tree
+
+
+tree_test = encode("abracadabra")
+assert tree_test == {'left0': {'symbol': 'a'},
+                     'right1': {'left0': {'symbol': 'b'},
+                                'right1': {'left0': {'symbol': 'r'},
+                                           'right1': {'left0': {'symbol': 'c'},
+                                                      'right1': {'symbol': 'd'}}}}}
+assert _encoding.generate_codes(tree_test) == ['a', '0', 'b', '10', 'r', '110', 'c', '1110', 'd', '1111']
